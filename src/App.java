f04163df26959913f4cb3ffcc0ec9f871e3f1e0f
Johancap115 <johancap115@gmail.com>
2021-08-20 20:12:34 -0500

    Ejecucion por terminal/consola

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,13 +9,31 @@ import javafx.stage.Stage;
 public class App extends Application {
     public static void main(String[] args) throws Exception {
         // System.out.println("Hello, World!");
+
+        float[][] matrizNotas;
+        int Registros;
+        float acumulado = 0;
+        GradingSystem sgs = new GradingSystem();
+        sgs.loadData();
+        Registros = Integer.parseInt(sgs.getDato());
+        matrizNotas = new float[Registros][4];
+        for (int i = 0; i < matrizNotas.length; i++) {
+            for (int j = 0; j < matrizNotas[i].length; j++) {
+                sgs.loadData();
+                matrizNotas[i][j] = Float.parseFloat(sgs.getDato());
+                acumulado += matrizNotas[i][3];
+                sgs.setAcumulado(acumulado);
+            }
+        }
+        sgs.setmSchoolNotas(matrizNotas);
+        sgs.setPromedio(acumulado / Registros);
+        sgs.resolverPreguntas();
+        System.out.println(String.format("%s \n%s \n%s \n%s", sgs.stat1(), sgs.stat2(), sgs.stat3(), sgs.stat4()));
         launch(args);
     }
 
-
     @Override
     public void start(Stage stage) throws Exception {
-        // TODO Auto-generated method stub
         FXMLLoader loader = new FXMLLoader(App.class.getResource("GUIReto3.fxml"));
         Parent ventana = (Pane) loader.load();
         Scene scene = new Scene(ventana);
@@ -22,4 +41,5 @@ public class App extends Application {
         stage.setTitle("Sistema Estadistico");
         stage.show();
     }
+
 }
