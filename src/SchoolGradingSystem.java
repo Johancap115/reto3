dbb417f957a7c7352c139c49867870ab0b62836f
Johancap115 <johancap115@gmail.com>
2021-08-20 20:14:42 -0500

    Implementacion del metodo loadData para uso de GUI

--- src/SchoolGradingSystem.java
+++ src/SchoolGradingSystem.java
@@ -1,5 +1,23 @@
-public class SchoolGradingSystem extends GradingSystem{
-    public void loadData(){
+public class SchoolGradingSystem extends GradingSystem {
+
+    public void loadData(String data, int n) {
+        
+        this.setmSchoolNotas(new float[n][4]);
+        // matrizNotas = new float[Registros][4];
+        float acumulado = 0;
+        float[][] matrizNotas = this.getmSchoolNotas();
+        String[] filasData = data.split("\n");
+        for (int i = 0; i < n; i++) {
+            String[] datoAtomico = filasData[i].split(" ");
+            for (int j = 0; j < matrizNotas[i].length; j++) {
+                matrizNotas[i][j] = Float.parseFloat(datoAtomico[j]);
+                acumulado += matrizNotas[i][3];
+                this.setAcumulado(acumulado);
+            }
+        }
+        this.setmSchoolNotas(matrizNotas);
+        this.setPromedio(acumulado/n);
+        //this.resolverPreguntas();
         
     }
 }
